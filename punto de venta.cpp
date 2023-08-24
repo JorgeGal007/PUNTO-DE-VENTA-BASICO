f7de90cd52bfbb18a0520c74a626cb8d8eb21eb4
Jorge Galindo <jorgegal0704@gmail.com>
2023-08-24 16:19:36 -0600

    Creando el menú de administrador

--- punto de venta.cpp
+++ punto de venta.cpp
@@ -8,6 +8,12 @@ using namespace std;
 void pantallaPrincipal();
 void pantallaAdmin();
 void pantallaVendedor();
+void altaProducto();
+void bajaProducto();
+void modProducto();
+void altaUsu();
+void bajaUsu();
+void modUsu();
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
@@ -33,7 +39,7 @@ void pantallaPrincipal(){
 		switch(opt){
 			case 1: pantallaAdmin(); break;
 			case 2: pantallaVendedor(); break;
-			case 3: break;
+			case 3: cout<<"Vuelva pronto :)"<<endl; break;
 			
 			default: cout<<"Por favor ingresa un valor aceptado"<<endl;
 		}
@@ -43,8 +49,53 @@ void pantallaPrincipal(){
 }
 
 void pantallaAdmin(){
+	int opt;
+	
+	do{
+		cout<<"\n¿Qué deseas hacer?"<<endl;
+		cout<<endl;
+		cout<<"1. Alta de producto."<<endl;
+		cout<<"2. Baja de producto."<<endl;
+		cout<<"3. Modificar producto."<<endl;
+		cout<<"4. Alta de usuario."<<endl;
+		cout<<"5. Baja de usuario."<<endl;
+		cout<<"6. Modificación de usuario."<<endl;
+		cout<<"7. Regresar al menú principal."<<endl;
+		cin>>opt;
+		
+		switch(opt){
+			case 1: altaProducto(); break;
+			case 2: bajaProducto(); break;
+			case 3: modProducto(); break;
+			case 4: altaUsu(); break;
+			case 5: bajaUsu(); break;
+			case 6: modUsu(); break;
+			case 7: pantallaPrincipal(); break;
+			
+			default: cout<<"Ingrese una opción válida"<<endl;
+		}
+	}while(opt != 7);
+	
 	cout<<endl;
 	cout<<"En construcción"<<endl;
+}
+
+void altaProducto(){
+}
+
+void bajaProducto(){
+}
+
+void modProducto(){
+}
+
+void altaUsu(){
+}
+
+void bajaUsu(){
+}
+
+void modUsu(){
 }
 
 void pantallaVendedor(){
